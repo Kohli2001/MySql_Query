b9c58db80518875b00e885477587d36817208288
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-07-26 17:37:39 +0530

    date functions

---
+++ Sql-Queries/Q11.sql
@@ -0,0 +1,19 @@
+use test;
+
+
+-- Dtae function
+
+select ename , sal , sal+comm as tot from emp;
+select * from emp;
+
+
+select * from emp 
+where convert(current_date , date) = 'dec' ;
+
+select * from emp 
+
+
+select CONVERT("2017-08-29", DATE);-- 
+
+select current_user();
+
